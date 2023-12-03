ef9fc62bb7342c64d37e7945950e393bca099b4a
Tommy Bautista <tommybautista320@gmail.com>
2023-12-03 08:10:13 -0500

    modified

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schemas/book.schema';
 
 @Controller('book')
-export class BookController {}
+export class BookController {
+
+    constructor(private bookService: BookService) {}
+
+    @Get()
+    async getAllBooks(): Promise<Book[]> {
+        return this.bookService.findAll();
+    }
+}
