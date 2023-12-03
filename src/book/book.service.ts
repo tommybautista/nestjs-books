2648aa27182b6256d9718cef6e35d419977891b7
tommyb320 <tommybautista320@gmail.com>
2023-12-03 07:36:05 -0500

    modified

---
+++ src/book/book.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Book } from './schemas/book.schema';
+
+@Injectable()
+export class BookService {
+
+    constructor(
+        @InjectModel(Book.name)
+        private bookModel: mongoose.Model<Book>
+    ) {}
+
+
+    async findAll(): Promise<Book []> {
+        const books = await this.bookModel.find()
+        return books
+    }
+}
