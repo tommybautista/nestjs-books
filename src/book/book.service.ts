f75614d0d8479d95b304a6d13c66f06d4f2ff5f2
tommyb320 <tommybautista320@gmail.com>
2023-12-05 13:42:35 -0500

    added search and pagination

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 import { Book } from './schemas/book.schema';
+import { Query } from 'express-serve-static-core';
 
 @Injectable()
 export class BookService {
@@ -12,8 +13,21 @@ export class BookService {
     ) {}
 
 
-    async findAll(): Promise<Book []> {
-        const books = await this.bookModel.find()
+    async findAll(query: Query): Promise<Book []> {
+
+        const resPerPage = 2;
+        const currentPage = Number (query.page) || 1;
+        const skip = resPerPage * (currentPage - 1);
+
+        const keyword = query.keyword ? {
+            title: {
+                $regex: query.keyword,
+                $options: 'i'
+            }
+        } : {}
+        const books = await this.bookModel.find({ ...keyword })
+        .limit(resPerPage)
+        .skip(skip);
         return books
     }
 
