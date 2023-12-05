d72fe116e87d8ef7b36831b3ad11c42e95485e1b
tommybautista <100245701+tommybautista@users.noreply.github.com>
2023-12-05 08:18:31 -0500

    modified

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+
+import { Query as ExpressQuery } from 'express-serve-static-core';
 
 @Controller('books')
 export class BookController {
@@ -10,8 +12,8 @@ export class BookController {
     constructor(private bookService: BookService) {}
 
     @Get()
-    async getAllBooks(): Promise<Book[]> {
-        return this.bookService.findAll();
+    async getAllBooks(@Query() query: ExpressQuery ): Promise<Book[]> {
+        return this.bookService.findAll(query);
     }
 
     @Post('new')
